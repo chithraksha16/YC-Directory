2e1b4774eee3949a4d9257024a037fbf6970c5c8
chithraksha16 <chithrakshakharvi@gmail.com>
2026-01-14 18:37:04 +0530

    initial stage

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { auth,signIn,signOut } from '@/auth'
+
+const Navbar = async () => {
+    const session= await auth()
+  return (
+    <>
+    <div className='px-5 py-3 bg-white font-work-sans shadow-sm'>
+        <nav className='flex justify-between items-center'>
+        <Link href='/'>
+        <Image src="/YCDirectory.png" alt="logo" height={30} width={144}/>
+        </Link>
+
+        <div className='flex items-center gap-5 text-black'>
+            {session && session?.user?(
+                <>
+                <Link href={'/startup/create'}>Create</Link>
+                <form action={async()=>{
+                    "use server"
+                    await signOut({redirectTo:"/"})}}>
+                    <button type='submit'>Logout</button>
+                </form>
+                <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+                </Link>
+                </>
+            ):(
+                <form action={async()=>{
+                    "use server"
+                    await signIn('github')}}>
+                <button type='submit'>Login</button>
+                </form>
+            )}
+
+        </div>
+        </nav>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
